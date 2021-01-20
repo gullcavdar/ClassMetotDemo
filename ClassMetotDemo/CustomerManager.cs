8a77a00114efe92d78774765b3b9eaede991d9a5
gül <cavdargul@gmail.com>
2021-01-20 21:06:39 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + " " + customer.CustomerSurname + " " + customer.CustomerProfession + " bilgileri olan müşteri eklendi");
+        }
+
+
+        public void DeleteCostumer(Customer customer)
+        {
+            Console.WriteLine("{0} {1} müşteri silindi", customer.CustomerName, customer.CustomerSurname);
+        }
+
+
+        public void CustomerList(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.CustomerName + " " + customer.CustomerSurname + " " + customer.CustomerProfession);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.CustomerName = "Gül";
+            customer1.CustomerSurname = "Çavdar";
+            customer1.CustomerProfession = "Mühendis";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.CustomerName = "Esmanur";
+            customer2.CustomerSurname = "Çavdar";
+            customer2.CustomerProfession = "Öğrenci";
+
+
+            Customer[] customers = new Customer[] { customer1,customer2 };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Console.WriteLine("Ekle");
+            customerManager.AddCustomer(customer1);
+            Console.WriteLine("Sil");
+
+            customerManager.DeleteCostumer(customer2);
+            Console.WriteLine("Liste");
+
+            customerManager.CustomerList(customers);
+           
+
+        }
+    }
+    
+}
